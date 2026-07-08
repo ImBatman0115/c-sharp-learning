ad0ae18f19e2258c7c060d7010a22692545d07ce
ImBatman0115 <kaydenmayhew@gmail.com>
2026-07-08 12:58:01 -0600

    created a C# and .NET project. Wrote first lines of C#

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿// Console.WriteLine("Hello, World!");
+
+// Console.WriteLine("Congratulations!");
+// Console.Write(" ");
+// Console.Write("You wrote your first lines of code!");
+
+//WriteLine -> includes a line feed (appends a new line after the output)
+// Write -> Puts text on one line, no line feed.
+
+//Challenge: Write code the produces two lines of text
+Console.WriteLine("This is the first line.");
+Console.Write("This is the second line.");
+
