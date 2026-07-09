ad2e823bf63c9d7af84c4b6e5f6d661b4928857a
ImBatman0115 <kaydenmayhew@gmail.com>
2026-07-09 12:45:37 -0600

    Used Humanizer and updated the version

--- HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d35fdbb4d211487756ba4ff1772a7148a0fff20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e08c8fa852be4cbdef48b257366482c92a13cff")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
