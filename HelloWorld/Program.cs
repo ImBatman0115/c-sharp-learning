1acee68c2596d10c80d556b59714950ec211e6b0
ImBatman0115 <kaydenmayhew@gmail.com>
2026-07-13 10:45:12 -0600

    practiced literal string formatting and start of concatenation

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -58,25 +58,65 @@
 // Console.Write(temperature);
 // Console.Write(" celsius.");
 
-using Humanizer;
+//-----Adding a Package------
 
-Console.WriteLine("Quantities:");
-HumanizeQuantities();
+// using Humanizer;
 
-Console.WriteLine("\nDate/Time Manipulation:");
-HumanizeDates();
+// Console.WriteLine("Quantities:");
+// HumanizeQuantities();
 
-static void HumanizeQuantities()
-{
-    Console.WriteLine("case".ToQuantity(0));
-    Console.WriteLine("case".ToQuantity(1));
-    Console.WriteLine("case".ToQuantity(5));
-}
+// Console.WriteLine("\nDate/Time Manipulation:");
+// HumanizeDates();
 
-static void HumanizeDates()
-{
-    Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize());
-    Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
-    Console.WriteLine(TimeSpan.FromDays(1).Humanize());
-    Console.WriteLine(TimeSpan.FromDays(16).Humanize());
-}
+// static void HumanizeQuantities()
+// {
+//     Console.WriteLine("case".ToQuantity(0));
+//     Console.WriteLine("case".ToQuantity(1));
+//     Console.WriteLine("case".ToQuantity(5));
+// }
+
+// static void HumanizeDates()
+// {
+//     Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize());
+//     Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
+//     Console.WriteLine(TimeSpan.FromDays(1).Humanize());
+//     Console.WriteLine(TimeSpan.FromDays(16).Humanize());
+// }
+
+//-----Literal String Formatting and More-----
+
+//Escape character sequence - instruction to runtime to insert a special character
+// \n -> new line  \t -> new tab
+// Console.WriteLine("Hello\nWorld");
+// Console.WriteLine("Hello\tWorld");
+// //example of double quotations
+// Console.WriteLine("Hello \"World\"!");
+// //example of file path
+// Console.WriteLine("c:\\source\\repos");
+
+// Console.WriteLine("\n");
+// Console.WriteLine("Generating invoices for customer \"Contoso Corpz\" ...\n");
+// Console.WriteLine("Invoice: 1021\t\tComplete!");
+// Console.WriteLine("Invoice: 1022\t\tComplete!");
+// Console.Write("\nOutput Directory:\t");
+// //Verbatim string literal -- keeps all whitespace and characters without the need of escape backslash
+// //To do so use "@" before the literal string like below
+// //Console.WriteLine(@"    c:\source\repos
+//             //(this is where your very cool code goes)");
+
+// Console.Write(@"c:\invoices");
+
+// //Unicode escape characters
+// //Kon'nichiwa World
+// //Console.WriteLine("\n\u3053\u3093\u306B\u3061\u306F World!");
+
+// // To generate Japanese invoices:
+// // Nihon no seikyū-sho o seisei suru ni wa:
+// Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
+// // User command to run an application
+// Console.WriteLine(@"c:\invoices\app.exe -j");
+
+//-----Concatenation-----
+string firstName = "Bob";
+string message = "Hello " + firstName;
+Console.WriteLine(message);
