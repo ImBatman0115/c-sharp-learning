e346db31ecb69cb384b914de4e974d222b95e49c
ImBatman0115 <kaydenmayhew@gmail.com>
2026-07-14 10:19:00 -0600

    started a new project, studeent grading application

---
+++ GradingProject/Program.cs
@@ -0,0 +1,8 @@
+﻿//Student Grading application
+//To automate the calculation of current grades for each student in a class.
+
+// You're given a short list of four students and their five assignment grades.
+// Each assignment grade is expressed as an integer value, 0-100, where 100 represents 100% correct.
+// Final scores are calculated as an average of the five assignment scores.
+// Your application needs to perform basic math operations to calculate the final grades for each student.
+// Your application needs to output/display each student’s name and final score.
