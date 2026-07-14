54402a0c654d401657f790cd9e599fbe60d6005a
ImBatman0115 <kaydenmayhew@gmail.com>
2026-07-14 10:57:04 -0600

    initialized variables

--- GpaCalculator/Program.cs
+++ GpaCalculator/Program.cs
@@ -1,4 +1,21 @@
 ﻿//GPA Calculator
 //Uses students grades and credit hours taken to calculate their overall GPA.
 
+// You're given the student's name and class information.
+// Each class has a name, the student's grade, and the number of credit hours for that class.
+// Your application needs to perform basic math operations to calculate the GPA for the given student.
+// Your application needs to output/display the student’s name, class information, and GPA.
 
+//initialize variables
+string studentName = "Sophia Johnson";
+string course1Name = "English 101";
+string course2Name = "Algebra 101";
+string course3Name = "Biology 101";
+string course4Name = "Computer Science I";
+string course5Name = "Psychology 101";
+
+int course1Credit = 3;
+int course2Credit = 3;
+int course3Credit = 4;
+int course4Credit = 4;
+int course5Credit = 3;
