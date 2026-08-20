0a4620a224984e659c8b014f662b24f67987ecb6
ImBatman0115 <kaydenmayhew@gmail.com>
2026-08-20 10:51:23 -0600

    created a dice rolling game!

--- HelloWorld2/obj/Debug/net10.0/HelloWorld2.AssemblyInfo.cs
+++ HelloWorld2/obj/Debug/net10.0/HelloWorld2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f418b29c8f54a2d1420ccc40e337121d87645a4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e50697de73e29ac309d2ab89733d03e226038d83")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
