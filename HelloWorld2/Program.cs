bf20e0389603a4c317a5623f0d5cae7636d92ca3
ImBatman0115 <kaydenmayhew@gmail.com>
2026-08-25 10:14:22 -0600

    created a fraudlent id cathcing program

--- HelloWorld2/Program.cs
+++ HelloWorld2/Program.cs
@@ -63,7 +63,7 @@
 //     Console.WriteLine("Your subscription expires within a day!");
 //     discountPercentage = 20;
 //     Console.WriteLine($"Renew now and save {discountPercentage}%");
-    
+
 // } else if(daysUntilExpiration <= 5)
 // {
 // Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
@@ -83,22 +83,50 @@
 //     Console.WriteLine($"Renew now and save {discountPercentage}%");
 // }
 
-//Arrays!!
+//ARRAYS!!
 // string[] fraudulentOrderIDs = new string[3];
 // fraudulentOrderIDs[0] = "A123";
 // fraudulentOrderIDs[1] = "B456";
 // fraudulentOrderIDs[2] = "C789";
 //fraudulentOrderIDs[3] = "D000";
 
-string[] fraudulentOrderIDs = ["A123", "B456", "C789"];
+// string[] fraudulentOrderIDs = ["A123", "B456", "C789"];
 
-Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
-Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
-Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+// Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+// Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+// Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
 
-//reassigning values
-fraudulentOrderIDs[0] = "F000";
-Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+// //reassigning values
+// fraudulentOrderIDs[0] = "F000";
+// Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
 
-//length property (will tell you how manny items are in your array, starting at 1)
-Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+// //length property (will tell you how many items are in your array, starting at 1)
+// Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+
+//USING LOOPS
+// string[] names = ["Rowena", "Robin", "Bao"];
+// foreach (string name in names)
+// {
+//     Console.WriteLine(name);
+// }
+
+// int[] inventory = [200, 450, 700, 175, 250];
+// int sum = 0;
+// int bin = 0;
+// foreach (int items in inventory)
+// {
+//     sum += items;
+//     bin++;
+// Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+// }
+// Console.WriteLine($"We have {sum} items in inventory");
+
+//CHALLENGE: Report the order IDs that need further investigation
+string[] fraudulentIDs = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
+foreach (string id in fraudulentIDs)
+{
+    if (id.StartsWith("B"))
+    {
+        Console.WriteLine($"{id}");
+    }
+}
